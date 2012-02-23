5289e2197e522453f1ac89d0b07dd491bf3bbf7b
Tomohiro Minagawa <fusha01@hotmail.com>
2012-02-23 23:09:11 +0900

    書名が変わってなかったので．ついでにプログラムもちょっと変更

--- helloworld-c-putchar.c
+++ helloworld-c-putchar.c
@@ -3,5 +3,6 @@ int main( int argc, char *argv[] )
 	putchar('H');putchar('e');putchar('l');putchar('l');putchar('o');
 	putchar(',');putchar(' ');
 	putchar('w');putchar('o');putchar('r');putchar('l');putchar('d');
+	putchar('!');
 	return 0;
 }
