270bc533fcbed3b7b1b9b0a24f95e95e7193b514
Tomohiro Minagawa <fusha01@hotmail.com>
2012-03-17 02:48:57 +0900

    うまくいかないけど，今日はここまで．

--- wave_trick.cpp
+++ wave_trick.cpp
@@ -3,21 +3,20 @@
 #include <vector>
 
 int main( int argc, char** argv){
-  //std::vector<char> inpcm1;
+
   std::ifstream infile1;
-  char * inpcm1;
   int length;
 
   infile1.open( argv[0], std::ifstream::binary );
   infile1.seekg(0, std::ios::end);
   length = infile1.tellg();
-  infile1.seekg(0, std::ios::beg);
+  infile1.seekg(44, std::ios::beg);
   std::cout << length << std::endl;
 
-  inpcm1 = new char [length];
-  infile1.read(inpcm1,length);
-
-  std::cout.write(inpcm1,length);
+  std::vector<char> inpcm1;  
+  infile1.read(&inpcm1[0],(length - 44));
+    
+  std::cout << inpcm1[0] << std::endl;
   
   infile1.close();
 
