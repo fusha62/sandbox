4824c49daa2ff2d35001f15b0d1b25da51dc4633
Tomohiro Minagawa <fusha01@hotmail.com>
2012-03-17 14:05:47 +0900

    argvの指定を間違えていたので，修正した．

--- wave_trick.cpp
+++ wave_trick.cpp
@@ -7,7 +7,7 @@ int main( int argc, char** argv){
   std::ifstream infile1;
   int length;
 
-  infile1.open( argv[0], std::ifstream::binary );
+  infile1.open( argv[1], std::ifstream::binary );
   infile1.seekg(0, std::ios::end);
   length = infile1.tellg();
   infile1.seekg(44, std::ios::beg);
@@ -15,7 +15,7 @@ int main( int argc, char** argv){
 
   std::vector<char> inpcm1;  
   infile1.read(&inpcm1[0],(length - 44));
-    
+  
   std::cout << inpcm1[0] << std::endl;
   
   infile1.close();
