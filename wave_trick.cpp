9e2fedd961c4962a95e9537ab5e7b3564d31e6bc
Tomohiro Minagawa <fusha01@hotmail.com>
2012-03-17 18:28:45 +0900

    夕飯を食べに行くので，とりあえずコミット．

--- wave_trick.cpp
+++ wave_trick.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 #define FORMAT_OFFSET 44
 
 int main( int argc, char** argv){
 
-  std::ifstream infile1;
+  std::ifstream infile1,infile2;
   int length;
+  int len_tmp1,len_tmp2;
 
+  /* file open */
   infile1.open( argv[1], std::ifstream::binary );
   infile1.seekg(0, std::ios::end);
-  length = infile1.tellg();
-  length = length - FORMAT_OFFSET;
+  len_tmp1 = infile1.tellg();
   infile1.seekg(FORMAT_OFFSET, std::ios::beg);
-  //std::cout << length << std::endl;
+
+  infile2.open( argv[2], std::ifstream::binary );
+  infile2.seekg(0, std::ios::end);
+  len_tmp2 = infile2.tellg();
+  infile2.seekg(FORMAT_OFFSET, std::ios::beg);
+
+  /* set length */
+  length = std::max(len_tmp1,len_tmp2);
+  length = length - FORMAT_OFFSET;
+
+  std::cout << length << std::endl;
 
   std::vector<char> inpcm1(length);
-  infile1.read(&inpcm1[0],length);
   
-  std::cout.write(&inpcm1[0],length);
+  infile1.read(&inpcm1[0],length);
+
+  //std::cout.write(&inpcm1[0],length);
   
   infile1.close();
 
